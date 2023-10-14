78d10cd95e7f20f441c2f3650440d8b9abf0543a
Shoghik <shoghik.kachatryan@dci-student.org>
2023-10-14 15:54:33 +0200

    first commit

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year % 4) {
+            case 0:
+                if (year % 400 != 0 && year % 100 == 0) {
+                    System.out.println("not leap");
+                } else {
+                    System.out.println("leap");
+                }
+                break;
+            default:
+                System.out.println("not leap");
+        }
     }
 }
